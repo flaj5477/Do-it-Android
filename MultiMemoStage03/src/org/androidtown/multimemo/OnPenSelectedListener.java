cd8d753ad7eb62f1ba6310c07ab1d8ad4d852a05
flaj5477 <flaj5477@daum.net>
2019-09-08 05:35:46 +0900

    안드로이드 1,2,3 스탭 파일 추가

---
+++ MultiMemoStage03/src/org/androidtown/multimemo/OnPenSelectedListener.java
@@ -0,0 +1,17 @@
+package org.androidtown.multimemo;
+
+/**
+ * ∆Ê ±Ω±‚ º±≈√ ∏ÆΩ∫≥ 
+ *
+ * @author Mike
+ */
+public interface OnPenSelectedListener {
+
+	/**
+	 * Called when a pen is selected
+	 *
+	 * @param pen
+	 */
+	public void onPenSelected(int pen);
+
+}
---
+++ QuickNaviStage04/gen/org/androidtown/quicknavi/stage05/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.androidtown.quicknavi.stage05;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
